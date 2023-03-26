e8e2c72e7f28fa611b0c4b616b5cc06a148490f5
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-03-26 11:20:15 -0300

    feat: notification class finalization

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,7 +1,9 @@
 export interface NotificationProps {
+  recipientId: string;
   content: string;
   category: string;
   readAt?: Date | null;
+  createdAt: Date;
 }
 
 export class notification {
@@ -11,11 +13,39 @@ export class notification {
     this.props = props;
   }
 
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
   public set content(content: string) {
     this.props.content = content;
   }
 
   public get content(): string {
     return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date | null | undefined {
+    return this.props.createdAt;
   }
 }
